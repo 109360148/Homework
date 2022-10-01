a76f6940ff5aaefa61a929d9ff369b16ee0c16a3
Wei <t109360148@ntut.org.tw>
2022-10-01 18:35:28 +0800

    Homework CH1

--- Sample10.java
+++ Sample10.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class Sample10 {
 	public static void main(String[] args) throws IOException
 	{
